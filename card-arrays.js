8a60e7c2df2f19c80f994741cf091fda0c477160
rob4path <bcrrobby@gmail.com>
2021-10-12 12:20:00 +0300

    idk

---
+++ card-arrays.js
@@ -0,0 +1,87 @@
+
+  const cardArrayProverbs = [
+    {
+      reference: "1 Ioan 2:6",
+      text: "1 Ioan 2:6",
+    },
+    {
+      reference: "1 Ioan 2:6",
+      text: "Cine zice că rămâne în El trebuie să trăiască și el cum a trăit Isus",
+    },
+    {
+      reference: "1 Ioan 1:9",
+      text: "1 Ioan 1:9",
+    },
+    {
+      reference: "1 Ioan 1:9",
+      text: "Daca ne marturisim pacatele El este credincios si drept ca sa ne ierte toate pacatele si sa ne ierte de orice nelegiuire",
+    },
+  ];
+  const cardArrayMore = [
+    {
+      reference: "....",
+      text: ". . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .... ...... ...... ..... ...... ... .... ....... .. ........................",
+    },
+    {
+      reference: "....",
+      text: ". . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .. ... ..   ..... .... . ..  . ...... ..............................",
+    },
+    {
+      reference: "OOOO",
+      text: "O O O O O O O O O O O O O O O O O O O O O O O O O O O O O O O O O O O O O O O O O O O O O O O O OOOO  OOOOO O OOOO  OOOO OOOOOO OOO OOOOOOOOOOOOOOOOOOOOOOOOO",
+    },
+    {
+      reference: "OOOO",
+      text: "O O O O O O O O O O O O O O O O O O O O O O O O O O O O O O O O O O O O O O O O O O O O O O O O  OOOOOOOO  OOOOO OO OOO OOO OO OOOOOOOOOOOOOOOOOOOOOOOOO",
+    },
+  ];
+  const cardArrayFAST = [
+    {
+      reference: "Ps. 143:8",
+      text: "Ps. 143:8",
+    },
+    {
+      reference: "Ps. 143:8",
+      text: "Fă-mă să aud dis-de-dimineaţă bunătatea Ta, căci mă încred în Tine. Arată-mi calea pe care trebuie să umblu, căci la Tine îmi înalţ sufletul.",
+    },
+    {
+      reference: "Romani 4:20",
+      text: "Romani 4:20",
+    },
+    {
+      reference: "Romani 4:20",
+      text: "El nu s-a îndoit de făgăduinţa lui Dumnezeu, prin necredinţă, ci, întărit prin credinţa lui, a dat slavă lui Dumnezeu,",
+    },
+    {
+      reference: "Ioan 15:7",
+      text: "Ioan 15:7",
+    },
+    {
+      reference: "Ioan 15:7",
+      text: "Dacă rămâneţi în Mine şi dacă rămân în voi cuvintele Mele, cereţi orice veţi vrea, şi vi se va da.",
+    },
+    {
+      reference: "1 Timotei 4:15",
+      text: "1 Timotei 4:15",
+    },
+    {
+      reference: "1 Timotei 4:15",
+      text: "Pune-ţi pe inimă aceste lucruri, îndeletniceşte-te în totul cu ele, pentru ca înaintarea ta să fie văzută de toţi.",
+    },
+    {
+      reference: "Proverbe 13:4",
+      text: "Proverbe 13:4",
+    },
+    {
+      reference: "Proverbe 13:4",
+      text: "Leneşul doreşte mult, şi totuşi, n-are nimic, dar cei harnici se satură. -",
+    },
+    {
+      reference: "2 Corinteni 8:11",
+      text: "Isprăviţi, dar, acum de făcut; pentru ca, după graba voinţei, să fie şi înfăptuirea, potrivit cu mijloacele voastre.",
+    },
+    {
+      reference: "2 Corinteni 8:11",
+      text: "2 Corinteni 8:11",
+    }
+  ];
